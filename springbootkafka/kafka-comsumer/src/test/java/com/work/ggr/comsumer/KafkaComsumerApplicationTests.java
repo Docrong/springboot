30fd9cc892ff118633304b97bc9436419b1fe936
guoguirong <hgguirong@qq.com>
2019-09-19 16:37:16 +0800

    添加子项目

---
+++ springbootkafka/kafka-comsumer/src/test/java/com/work/ggr/comsumer/KafkaComsumerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.work.ggr.comsumer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KafkaComsumerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
---
+++ springbootkafka/src/main/java/com/work/ggr/kafkacomsumer/KafkaComsumerApplication.java
@@ -0,0 +1,13 @@
+package com.work.ggr.kafkacomsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaComsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaComsumerApplication.class, args);
+    }
+
+}
