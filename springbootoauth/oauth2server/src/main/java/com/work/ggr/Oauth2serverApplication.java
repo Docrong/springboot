f350b734898b02d4f71a284e9600d573d983b12d
郭贵荣 <guoguirong@boco.com.cn>
2019-10-21 23:07:48 +0800

    添加client和server

---
+++ springbootoauth/oauth2server/src/main/java/com/work/ggr/Oauth2serverApplication.java
@@ -0,0 +1,13 @@
+package com.work.ggr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2serverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2serverApplication.class, args);
+    }
+
+}
---
+++ springbootoauth/oauth2server/src/test/java/com/work/ggr/Oauth2serverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.work.ggr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2serverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
