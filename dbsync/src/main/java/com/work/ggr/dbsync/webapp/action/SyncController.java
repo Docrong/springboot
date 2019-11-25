dc73f8b3e45463698872b619fbc6d75d10ffd10f
guoguirong <1107719463@qq.com>
2019-11-25 17:58:30 +0800

    1

--- dbsync/src/main/java/com/work/ggr/dbsync/webapp/action/SyncController.java
+++ dbsync/src/main/java/com/work/ggr/dbsync/webapp/action/SyncController.java
@@ -1,5 +1,6 @@
 package com.work.ggr.dbsync.webapp.action;
 
+import com.work.ggr.dbsync.service.impl.SyncServiceImpl;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -19,26 +22,60 @@ import java.util.Map;
  */
 @RestController
 public class SyncController {
-    @Autowired
-    @Qualifier("mainDS")
-    DataSource mainDS;
-    @Autowired
-    @Qualifier("backupDS")
-    DataSource backupDS;
-    private Log log = LogFactory.getLog(this.getClass());
+	@Autowired
+	@Qualifier("mainDS")
+	DataSource mainDS;
+	@Autowired
+	@Qualifier("backupDS")
+	DataSource backupDS;
+	private Log log = LogFactory.getLog(this.getClass());
 
-    @RequestMapping("/test")
-    public String test(HttpServletRequest request, HttpServletResponse response) {
-        log.info(mainDS);
-        log.info(backupDS);
-        if (false) {
-            return "fail";
+	@Autowired
+	private SyncServiceImpl service;
+
+	@RequestMapping("/test")
+	public String test(HttpServletRequest request, HttpServletResponse response) {
+		log.info(mainDS);
+		log.info(backupDS);
+
+		List<String> maintable = service.getTablesName(mainDS);
+		List<String> backuptable = service.getTablesName(backupDS);
+		log.info(maintable);
+		log.info(backuptable);
+
+		List<String> xinzeng = new ArrayList();//需要新增的表
+		List<String> tongbu = new ArrayList();//需要数据同步的表
+		for (String str : maintable) {
+			if (backuptable.contains(str)) {
+				tongbu.add(str);
+			} else {
+				xinzeng.add(str);
+			}
+		}
+		log.info(xinzeng);
+		log.info(tongbu);
+
+		List<String> xinzengdesc = new ArrayList<String>();
+		for (String tablename : xinzeng) {
+			String desc = service.getTableDesc(tablename, mainDS);
+			xinzengdesc.add(desc);
+		}
+        for (String sql:xinzengdesc) {//将备份库不存在的表初始化一下
+            service.initTable(backupDS,sql);
         }
-        return "success";
-    }
+		log.info(xinzengdesc);
+		for (String tablename : xinzeng) {
+			List dataList = service.getData(mainDS, tablename);//获取表的数据
 
-    public Map initDsec(){
+		}
+        for (String tablename:tongbu) {
+            List dataList = service.getData(mainDS, tablename);
+        }
+		return "success";
+	}
 
-        return new HashMap();
-    }
+	public Map initDsec() {
+
+		return new HashMap();
+	}
 }
