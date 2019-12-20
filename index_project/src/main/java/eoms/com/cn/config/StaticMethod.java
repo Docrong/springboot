e8d3f13fbb06141474e44b65524b2b50ac1b0f21
guoguirong <1107719463@qq.com>
2019-12-20 11:24:20 +0800

    完成
    Signed-off-by: guoguirong <1107719463@qq.com>

---
+++ index_project/src/main/java/eoms/com/cn/config/StaticMethod.java
@@ -0,0 +1,12 @@
+package eoms.com.cn.config;
+
+/**
+ * @author : gr
+ * @date : 2019/12/20 10:12
+ */
+public class StaticMethod {
+
+	public static String  Null2String(String str) {
+		return str==null?"":str;
+	}
+}
