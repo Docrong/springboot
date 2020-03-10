af2fe1ab499e15c242641097bd766c32b2a4dee9
guoguirong <1107719463@qq.com>
2020-03-10 17:43:55 +0800

    db更改
    Signed-off-by: guoguirong <1107719463@qq.com>

---
+++ springbootdb/src/test/java/com/work/ggr/SpringbootdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.work.ggr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
