548e0187b9929976eda05c026da8edccf8fe6ef4
1107719463@qq.com <1107719463@qq.com>
2020-03-16 13:56:23 +0800

    eureka组件更改

--- springbooteureka/eurekaserver/src/test/java/com/work/ggr/eureka/server/EurekaserverApplicationTests.java
+++ springbooteureka/eurekaserver/src/test/java/com/work/ggr/eureka/server/EurekaserverApplicationTests.java
@@ -1,13 +1,14 @@
 package com.work.ggr.eureka.server;
 
+import org.testng.annotations.AfterTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EurekaserverApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
