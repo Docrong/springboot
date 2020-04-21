4223c85275aaea8331e4557394c8f3dedeba4c7a
guoguirong <hgguirong@qq.com>
2020-04-21 11:16:32 +0800

    添加springcloud config 配置中心

---
+++ springcloudconfig/configclient/src/main/java/com/work/ggr/configclient/ConfigClientApplication.java
@@ -0,0 +1,16 @@
+package com.work.ggr.configclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author gr
+ */
+@SpringBootApplication
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+}
