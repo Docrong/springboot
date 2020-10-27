e88e73d25adc97a228c24cfadeeb0ddbba7f6028
郭贵荣 <15000222148@139.com>
2020-10-27 15:09:22 +0800

    添加SpirngCloud-Alibaba-Nacos

---
+++ springbootalibaba/nacos-server2/src/test/java/com/work/ggr/nacosserver2/NacosServer2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.work.ggr.nacosserver2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacosServer2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
